5aa4886e99bf8ed91e8c759a7a8a992ea879c9bf
alexandra <alexandrabsouz@gmail.com>
2021-08-16 19:09:45 -0300

    feat: Implementing error in swapi_api_consumer and testing with mocks

--- src/infra/swapi_api_consumer.py
+++ src/infra/swapi_api_consumer.py
@@ -1,9 +1,49 @@
+from collections import namedtuple
+from typing import Type, Dict, Tuple
+
 import requests
+from requests import Request
+from src.errors import HttpRequestError
+
+
 class SwapiApiConsumer:
     
-    @classmethod
-    def get_starships(self, page: int) -> any:
-        params = {'page': page}
-        response = requests.get('https://swapi.dev/api/starships/', params=params)
+    ''' Class to consume swapi api with http requests '''
+    
+    def __init__(self) -> None:
+        self.get_starships_response = namedtuple('GET_Starships', 'status_code request response')
+    
+    
+    def get_starships(self, page: int) -> Tuple[int, Type[Request], Dict]:
+        req = requests.Request(
+            method='GET',
+            url='https://swapi.dev/api/starships/',
+            params={'page': page}
+        )
         
-        return response.json()
+        req_prepared = req.prepare()
+        
+        response = self.__send_http_request(req_prepared)
+        status_code = response.status_code
+        
+        if status_code >= 200 and status_code <= 299:
+            return self.get_starships_response(
+                status_code=response.status_code, request=req, response=response.json()
+            )
+            
+        else:
+            raise HttpRequestError(
+                message=response.json()['detail'], status_code=status_code
+            )
+        
+    @classmethod
+    def __send_http_request(cls, req_prepared: Type[Request]) -> any:
+        '''
+            Prepare a session and send http request
+            :param - req_prepare: Resquest Object with all params
+            :response - Http response raw
+        '''
+        http_session = requests.Session()
+        response = http_session.send(req_prepared)
+        return response
+        
--- src/infra/swapi_api_consumer_test.py
+++ src/infra/swapi_api_consumer_test.py
@@ -1,10 +1,33 @@
+from src.errors import HttpRequestError
+
 from .swapi_api_consumer import SwapiApiConsumer
+
 
 def test_get_starships(requests_mock):
     
-    requests_mock.get('https://swapi.dev/api/starships/', status_code = 200, json = ({'some': 'thing'}))
+    requests_mock.get('https://swapi.dev/api/starships/', status_code = 200, json = ({'some': 'thing', 'results': [{}]}))
     
     swapi_api_consumer = SwapiApiConsumer()
-    response = swapi_api_consumer.get_starships(page=1)
+    page = 1
+    get_starships_response = swapi_api_consumer.get_starships(page=page)
     
-    print(response)
+    assert get_starships_response.request.method == 'GET'
+    assert get_starships_response.request.url == 'https://swapi.dev/api/starships/'
+    assert get_starships_response.request.params == {'page': page}
+    
+    
+    assert get_starships_response.status_code == 200
+    assert isinstance(get_starships_response.response['results'], list)
+    
+    
+def test_get_starships_http_error(requests_mock):
+    
+    requests_mock.get('https://swapi.dev/api/starships/', status_code = 404, json = ({'detail': 'something', 'results': [{}]}))
+    
+    swapi_api_consumer = SwapiApiConsumer()
+    page = 100
+    try:
+        get_starships_response = swapi_api_consumer.get_starships(page=page)
+        assert True is False
+    except HttpRequestError as error:
+        assert error.message is not None
