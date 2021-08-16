faf131dfe137de2ed7faea9013e6d07792060597
alexandra <alexandrabsouz@gmail.com>
2021-08-16 17:49:28 -0300

    feat: Fist interaction

---
+++ src/infra/swapi_api_consumer_test.py
@@ -0,0 +1,7 @@
+from .swapi_api_consumer import SwapiApiConsumer
+
+def test_get_starships():
+    swapi_api_consumer = SwapiApiConsumer()
+    response = swapi_api_consumer.get_starships(page=10)
+    
+    print(response)
